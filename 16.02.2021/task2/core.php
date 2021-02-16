38bd8bb14420edfed6ec4388d36fd1e394634a70
Askerased <70854483+Askerased@users.noreply.github.com>
2021-02-16 20:34:16 +0400

    desc

--- 16.02.2021/task2/core.php
+++ 16.02.2021/task2/core.php
@@ -1,36 +1,39 @@
 <?php
 
 $a     = 1;
-$setir = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ];
+$setir = [ '', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ];
+
 for ( $i = 1; $i <= 8; $i++ )
 {
 	$sutun = 9 - $i;
-	echo "<tr class='classtr' id='$i'>";
+	echo "<tr id='$i'>";
 	echo "<td>$sutun</td>";
 
 	for ( $j = $a; $j < $a + 8; $j++ )
 	{
-
 		if ( $j % 2 === 0 )
 		{
-			if ( $a === 1 )
-			{
-				$yeni_j = $j-1;
-				echo "<td>$setir[$yeni_j]</td>";
-			}
 			echo "<td class='black' id='$j'></td>";
 		}
 		else
 		{
-			if ( $a === 1 )
-			{
-				$yeni_j = $j-1;
-				echo "<td>$setir[$yeni_j]</td>";
-			}
 			echo "<td class='white' id='$j'></td>";
 		}
+
 	}
 
 	echo '</tr>';
+
+	if ( $i === 8 )
+	{
+		echo '<tr>';
+
+		foreach ( $setir as $herf )
+		{
+			echo "<td>$herf</td>";
+		}
+
+		echo '</tr>';
+	}
 	$a += 9;
 }
